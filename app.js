518bb85dd57a104ef1237c7a808911f93607f1e8
石川宗 <ishikawa@nrsd.jp>
2020-03-24 02:54:25 +0900

    Node.js + Express sample Project

---
+++ app.js
@@ -0,0 +1,18 @@
+var systemlogger = require("./lib/log/systemlogger");
+var accesslogger = require("./lib/log/accesslogger");
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.disable("x-powered-by");
+
+app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
+
+app.use(accesslogger());
+
+app.use("/", require("./routes/index.js"));
+
+app.use(systemlogger());
+
+
+app.listen(3000);
